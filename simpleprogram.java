918b4e9946ceaf5949d3d5fddaae4a8a5a285225
Samim Hakimi <30713537+sammyHa@users.noreply.github.com>
2018-03-22 21:35:53 -0700

    Update simpleprogram.java

--- simpleprogram.java
+++ simpleprogram.java
@@ -1,30 +1,42 @@
 import java.util.Scanner;
-public class simpleprogram {
-    public static void main(String[] args) {
-       double c, f ;
+public class Exercise_05_01 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int positives = 0; 	// Count the number of positive numbers
+		int negatives = 0; 	// Count the number of negative numbers
+		int count = 0;			// Count all numbers
+		double total = 0;		// Accumulate a totol
 
 
-       //Converts F to C
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("please enter Far: ");
-        f = scanner.nextDouble();
-        c = (5.0/9.0 )* (f - 32);
+		// Promopt the user to enter an integer or 0 to exit
+		System.out.print("Enter an integer, the input ends if it is 0: ");
+		int number = input.nextInt();
 
-        System.out.println("celcuis = " + c);
+		if (number == 0) {	// Test for sentinel value
+			System.out.println("No numbers are entered except 0");
+			System.exit(1);
+		}
 
+		while (number != 0) {// Test for sentinel value
+			if (number > 0)
+				positives++;	// Increase positives
+			else
+				negatives++;	// Increase negatives
+			total += number;	// Accumulate total
+			count++;				// Increase the count
+			number = input.nextInt();
+		}
 
+		// Calculate the average
+		double average = total / count;
 
-        //Convert C to F
-        System.out.println("Please Enter C: " );
-        c = scanner.nextDouble();
-
-        f = c*(9.0/5.0)+32;
-        System.out.println("f = " + f);
-
-       int strawsOnCamel = 9;
-       strawsOnCamel = --strawsOnCamel;
-        System.out.println("strawsOnCamel = " + (int)(24.768 * 100)/100.0);
-
-    }
-
+		// Display results
+		System.out.println(
+			"The number of positive is " + positives +
+			"\nThe number of negatives is " + negatives +
+			"\nThe total is total " + total +
+			"\nThe average is " + average);
+	}
 }
+
